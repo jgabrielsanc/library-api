ebfef2975829c347d15593ff5e144bdde45d0e15
jgabrielsanc <jesusgabriel2525@gmail.com>
2020-12-06 15:15:36 -0400

    fixing some warnings
    changing versions to fix warnings related to some dependencies

--- library-services/src/main/java/com/gbh/library/config/LiquibaseConfig.java
+++ library-services/src/main/java/com/gbh/library/config/LiquibaseConfig.java
@@ -4,14 +4,12 @@ import liquibase.integration.cdi.CDILiquibaseConfig;
 import liquibase.integration.cdi.annotations.LiquibaseType;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import liquibase.resource.ResourceAccessor;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.sql.DataSource;
 
-@Slf4j
 @ApplicationScoped
 public class LiquibaseConfig {
 
@@ -21,7 +19,6 @@ public class LiquibaseConfig {
     @Produces
     @LiquibaseType
     public CDILiquibaseConfig cdiLiquibaseConfig() {
-        log.info("creating liquibase CDI config.");
         CDILiquibaseConfig config = new CDILiquibaseConfig();
         config.setChangeLog("db/changelog.sql");
         return config;
@@ -30,14 +27,12 @@ public class LiquibaseConfig {
     @Produces
     @LiquibaseType
     public DataSource dataSource() {
-        log.info("getting liquibase datasource.");
         return dataSource;
     }
 
     @Produces
     @LiquibaseType
     public ResourceAccessor resourceAccessor() {
-        log.info("getting liquibase resource accessor.");
         return new ClassLoaderResourceAccessor(getClass().getClassLoader());
     }
 }
